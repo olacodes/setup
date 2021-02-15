cf2e98d17da5cfe1387ed517c5c182a3c7a20b66
Olatunde Sodiq <olatundesodiq@gmail.com>
2021-02-15 18:00:34 +0100

    fix ci workflow

--- chitChat/settings.py
+++ chitChat/settings.py
@@ -12,7 +12,8 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 from decouple import config
-
+import os 
+print(os.environ.get('GITHUB_WORKFLOW'))
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
